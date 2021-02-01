2710e691083b97d77736721a564051a964883d4f
smitb383 <brooke.smith@outlook.com>
2021-02-01 13:43:43 -0500

    add park transitions

--- js/development.js
+++ js/development.js
@@ -7,12 +7,17 @@ var offsets = [];
 var eggs=[]; 
 let extraImages=[]; 
 let startingColor = []; 
+let eggTween= []; 
+let stopTween =false; 
+let targetPosition = new THREE.Vector3( 1.5, 1.5, 21.5); 
 
 const canvas = document.getElementById("myCanvas");
 const canvasContainer = document.getElementById("CC");
 const popUp = document.querySelectorAll(".popUp"); 
 let overlay = document.querySelector(".modalOverlay"); 
 let modelContent = document.querySelectorAll(".modalContent");
+let parkBlock = document.querySelectorAll(".parkContent"); 
+parkBlock[0].style.display="block"; 
 
 let amountScroll = 0; 
 const scrollMaxPres = 3500; 
@@ -21,6 +26,7 @@ const scrollMaxPres = 3500;
 const fov = 75;
 let  camStart = {x:0, y:250, z:0}; 
 let camMain = {x:0, y:250, z:0}; 
+let domEvents; 
 
 //START EVERYTHING WHEN LOADED
 window.addEventListener("load", init);
@@ -52,7 +58,7 @@ function init() {
   camera.position.set(camStart.x, camStart.y, camStart.z);
 
 //DOMEVENTS
-const domEvents = new THREEx.DomEvents(camera, renderer.domElement); 
+ domEvents = new THREEx.DomEvents(camera, renderer.domElement); 
   //CONTROLS
   controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
@@ -69,7 +75,7 @@ const domEvents = new THREEx.DomEvents(camera, renderer.domElement);
   makeGround(); 
 
   //ANIMATE CAMERA INTO SCENE
-//   cameraBegin(camera); 
+  // cameraBegin(camera); 
  //ALL EVENT LISTENERS 
  watchEvents(domEvents); 
   animate();
@@ -79,6 +85,10 @@ function watchEvents(domEvents){
   //EGG CLICK TRIGGERS 
   eggs.forEach((egg, index)=> {
     domEvents.addEventListener(eggs[index], "click", function(event){
+      TWEEN.removeAll();
+      parkBlock[0].style.displ
+      if(index =1){
+        parkBlock[0].style.display="block"; }
         clickEgg(eggs[index], offsets[index], index); 
       }); 
   }); 
@@ -87,36 +97,63 @@ function watchEvents(domEvents){
   }); 
   eggs.forEach((egg, index)=> {
     domEvents.addEventListener(eggs[index], "mouseover", function(event){
-        eggs[index].material.color.setHex(0xFF0000); 
+      console.log("mouse Over"); 
+        eggs[index].material.color.setHex(0xCDC2F6); 
+        TWEEN.removeAll();
+        new TWEEN.Tween(eggs[index].scale )
+        .to( new THREE.Vector3(2.2, 2.2, 2.2), 500 )
+      .easing( TWEEN.Easing.Cubic.InOut )
+      .start();
       }); 
   }); 
   eggs.forEach((egg, index)=> {
     domEvents.addEventListener(eggs[index], "mouseout", function(event){
+      console.log("mouseOut"); 
         eggs[index].material.color.setHex(startingColor[index]); 
+        console.log(eggs[index].scale);
+        new TWEEN.Tween(eggs[index].scale )
+        .to( new THREE.Vector3(1, 1, 1), 500 )
+      .easing( TWEEN.Easing.Cubic.InOut )
+      .start();
+      eggs[index].scale.x = 1; 
+        eggs[index].scale.y = 1; 
+        eggs[index].scale.z= 1; 
+          tweenEggs(); 
       }); 
   }); 
   window.addEventListener("resize", onWindowResize, true);
     //scrolling effect that switches museum to torpedo
-    popUp[0].onwheel = scrollHistorical; 
-
-    var targetPosition = new THREE.Vector3( 2, 2, 2 );
+    popUp[0].onwheel = scrollHistorical;   
     //ANIMATE THE EGGS
-    eggs.forEach((egg, index)=> {
-        new TWEEN.Tween( eggs[index].scale )
-        .to( targetPosition, 1500 )
-      .repeat( Infinity )
-      .easing( TWEEN.Easing.Cubic.InOut )
-      .start();
-      }); 
-    
+    tweenEggs();  
+}
+function selectedEgg(){
+  //tween one hover and clicked eggs 
+}
+function tweenEggs(){
+  console.log("tweening eggs again")
+  eggs.forEach((egg, index)=> {
+    if (eggs[index].scale.x >= 1.4){
+      targetPosition = new THREE.Vector3( 1, 1, 1 );
+     }
+    else{
+       targetPosition = new THREE.Vector3( 1.5, 1.5, 1.5 );
+    }
+    eggTween[index] = new TWEEN.Tween( eggs[index].scale )
+     .to( targetPosition, 1500 )
+   .repeat( Infinity )
+   .yoyo(true)
+   .easing( TWEEN.Easing.Cubic.InOut)
+   .start();
+   }); 
 }
 function scrollHistorical(event){
     if(amountScroll >= scrollMaxPres){
       amountScroll = scrollMaxPres; 
-      console.log("max"); 
+      // console.log("max"); 
       popUp[0].style.color ='#F9ECDC';
-      clearModal(); 
-      clickEgg(nhTrig, offsets[5], 5); 
+      clearModal(true); 
+      clickEgg(nhTrig, offsets[5], 5, true); 
       amountScroll =0; 
     }
     else if(amountScroll <4000 && amountScroll> 0){
@@ -125,45 +162,60 @@ function scrollHistorical(event){
    
     else if(amountScroll <=0){
       amountScroll = 0; 
-      console.log("min"); 
+      // console.log("min"); 
       popUp[0].style.color ='#B13929';
     }
     // console.log("amount Scroll " + amountScroll); 
     amountScroll+=event.deltaY; 
     // console.log(event.deltaY); 
   }
-
   function clickBack(index, event){
-    console.log("back to museum!")
-    clickEgg(mapItems[index], offsets[index], index); 
+    // stopTween=true; 
+    TWEEN.removeAll();
+    clickEgg(mapItems[index], offsets[index], index, true); 
   }
-
-function showParks(){
-    let position = new THREE.Vector3(-50, 150, 0); 
+function showParks1(){
+    let position = new THREE.Vector3(0, 200, 0); 
+    parkBlock[0].style.display="none"; 
     parkTween(camera, position, 2000); 
 }
 function parkTween(camera, position, duration) {    
     let coords = { x: camera.position.x, y: camera.position.y, z: camera.position.z };
-    // console.log("camera x " + coords.x + " camera y " + coords.y + " camera z " + coords.z); 
   var tween3 = new TWEEN.Tween(camera.position)
   .to({x:position.x, y: position.y, z: position.z}, 1500)
   .easing(TWEEN.Easing.Quadratic.In)
   .onComplete(() =>{
-      scene.add(parkAdd1);
-      mapPlane.material.opacity = .5;  })
-  .start();
-  
-  }
-function clickEgg(clicked, offset, index){
-  //TWEEN TO LOCATION/ ZOOM
-  clearModal(); 
-  let moveTO = new THREE.Vector3(clicked.position.x + offset.x, clicked.position.y + offset.y, clicked.position.z + offset.z)  
-  tweenCamera(camera,moveTO, 3500, clicked, index );
+console.log(parkAdd1.material.opacity); 
+
+      new TWEEN.Tween(parkAdd1.material ).to( { opacity:1 }, 1000 ).onComplete(function(){
+          parkAdd1.material.opacity = 1; 
+          console.log(parkAdd1.material.opacity); 
+      }).start();
+      new TWEEN.Tween(mapPlane.material ).to( { opacity: .5 }, 1000 ).start();
     
+    })
+  .start();
+  // parkAdd1.material.opacity = 1; 
+  }
+function clickEgg(clicked, offset, index, clear){
+  //TWEEN TO LOCATION/ ZOOM
+  clearModal(clear); 
+  TWEEN.removeAll();
+  //change the appearnace of clicked v not clicked things
+clicked.material.color.setHex(startingColor[index]); 
+mapItems.forEach((mapItem, index) =>{
+  mapItem.scale = new THREE.Vector3(1, 1, 1); 
+})
+clicked.scale.x =2; 
+clicked.scale.y =2; 
+clicked.scale.z =2; 
+
+clicked.material.color.setHex(startingColor[index]); 
+  let moveTO = new THREE.Vector3(clicked.position.x + offset.x, clicked.position.y + offset.y, clicked.position.z + offset.z)  
+  tweenCamera(camera,moveTO, 3500, clicked, index );  
 }
 
 function tweenCamera(camera, position, duration, object, index) {   
-
   new TWEEN.Tween(camera.position).to({
     x: position.x, y: position.y , z:position.z 
   }, 1500)
@@ -175,23 +227,25 @@ function tweenCamera(camera, position, duration, object, index) {
     overlay.addEventListener("click", clearPopUp, false);  
   })
   .start();
-  console.log(camera.position)
 }
 function tweenCameraBack(camera, position, duration) {    
   let coords = { x: camera.position.x, y: camera.position.y, z: camera.position.z };
-  // console.log("camera x " + coords.x + " camera y " + coords.y + " camera z " + coords.z); 
   var tween2 = new TWEEN.Tween(camera.position)
   .to({x:position.x, y: position.y, z: position.z}, 1500)
   .easing(TWEEN.Easing.Quadratic.In)
   .start();
-
 }
-function clearModal(){
-  overlay.style.display ="none"; 
-  console.log("you exited a modal"); 
-scene.remove(extraImages[0]); 
-mapPlane.material.opacity = 1;
-  extraImages.forEach((extraImage)=>{scene.remove(extraImage)}); 
+function clearModal(overlayDisplay){
+  if (overlayDisplay==true){
+    overlay.style.display ="block"; 
+    overlay.style.opacity ="0.001"; 
+  }
+  else{
+    overlay.style.display ="none"; 
+  }
+  parkAdd1.material.opacity = 0; 
+  mapPlane.material.opacity = 1;
+  // extraImages.forEach((extraImage)=>{scene.remove(extraImage)}); 
   modelContent.forEach((modelContent)=>{modelContent.scrollTop = 0}); 
     popUp.forEach((popUp, index)=> {
       popUp.style.display = "none";
@@ -199,22 +253,21 @@ mapPlane.material.opacity = 1;
     amountScroll = 0; 
 }
 function clearPopUp(){
- clearModal()
+ clearModal(false)
+ console.log("clear popups" )
   overlay.removeEventListener("click", clearPopUp, false); 
-  console.log("preTween position " + camera.position.x + " y " + camera.position.y + " z" + camera.position.z); 
   tweenCameraBack(camera,camMain, 3000); 
+  //make easter eggs jump again
+  tweenEggs(); 
 }
-
 function cameraBegin(camera){
   let coords = { x: camera.position.x, y: camera.position.y, z: camera.position.z };
-  // console.log("camera x " + coords.x + " camera y " + coords.y + " camera z " + coords.z); 
   var tween = new TWEEN.Tween(coords)
   .to({x:camMain.x, y:camMain.y, z:camMain.z}, 2500)
   .delay(1000) 
   .easing(TWEEN.Easing.Quadratic.In)
   .onUpdate(() =>{
     camera.position.set(camera.position.x, coords.y, camera.position.z);
-    // console.log("updated camera " + "camera x " + coords.x + " camera y " + coords.y + " camera z " + coords.z); 
     camera.lookAt(scene.position)
   })
   .start();
@@ -244,71 +297,48 @@ function makeClouds(){
 function setUpCubes(){
 const water = new THREE.TextureLoader().load("/media/water.jpg");
 const musImg = new THREE.TextureLoader().load("/media/museum.png");
-    //ADD MUSUEM CUBE
-  var musGeometry = new THREE.PlaneGeometry(15, 10);
-  var musMaterial = new THREE.MeshStandardMaterial({
-    map: musImg,
-    transparent: true,
-  });
-  mus = new THREE.Mesh(musGeometry, musMaterial);
-  eggs[0] = mus; 
-  mus.rotation.x = -90 * (Math.PI / 180);
-  mus.position.y = 3;
-  scene.add(mus);
-  mus.position.set(-23, 5, 26);
-  mapItems[0] = mus;
+//ADD MUSUEM CUBE
   offsets[0] =  {x: 5, y: 50, z: 4}; 
-
+   let pos0 = {x: -23,  y:5, z:26 }
+   let size0 = {x: 15, y: 10}; 
+  setUpEgg(0, size0, mus, pos0,musImg); 
   //ADD PARK CUBE
-  park = new THREE.Sprite(new THREE.SpriteMaterial({alphaMap: mapPlane, map: musImg, trasparent: true,  side: THREE.DoubleSide,depthWrite: true }));
-// park.rotation.x = -90 * (Math.PI / 180);
-  eggs[1] = park; 
-  scene.add(park);
-  mapItems[1] = park;
-  park.scale.set(20, 10,20); 
-  park.position.set(-30, 4, -100);
   offsets[1] = {x: 5, y: 50, z: 5}; 
+  let pos1 = {x: -30,  y:4, z:-100 }
+  let size1 = {x: 15, y: 10}; 
+ setUpEgg(1, size1, park, pos1,musImg);
 
 //EXTRA PARK STUFF
-const parksImg = new THREE.TextureLoader().load("/media/map_1.png");
-    var parkP = new THREE.PlaneGeometry(200, 300);
-    var parkM = new THREE.MeshStandardMaterial({ map: parksImg, transparent: true});
+  const parksImg = new THREE.TextureLoader().load("/media/map_1.png");
+  var parkP = new THREE.PlaneGeometry(200, 300);
+  var parkM = new THREE.MeshStandardMaterial({ map: parksImg, transparent: true,  
+    side: THREE.DoubleSide,
+    depthWrite: false, }); 
+   
   parkAdd1 = new THREE.Mesh(parkP,parkM);
   parkAdd1.rotation.x = -90 * (Math.PI / 180);
   parkAdd1.position.y = 1;
+  // parkAdd1.visible = false; 
   extraImages[0] = parkAdd1
+  scene.add(parkAdd1); 
+  parkAdd1.material.opacity = 0;
 //GEOLOGY 
-  var geoGeometry = new THREE.PlaneGeometry(15, 10);
-  var geoMaterial = new THREE.MeshStandardMaterial({
-    map: musImg,
-    transparent: true,
-  });
-  geo = new THREE.Mesh(geoGeometry, geoMaterial);
-  geo.rotation.x = -90 * (Math.PI / 180);
-  mus.position.y = 3;
-  scene.add(geo);
-  geo.position.set(20, 4, -60);
-  mapItems[2] = geo;
-  offsets[2] =  {x: 30, y: 40, z: -10}; 
-  eggs[2] = geo; 
+  offsets[2] =  {x: 10, y: 40, z: -10}; 
+  let pos2 = {x: 20,  y:4, z:-60}
+  let size2 = {x: 15, y: 10}; 
+ setUpEgg(2, size2, geo, pos2,musImg);
 
-
-  com = new THREE.Sprite(new THREE.SpriteMaterial({alphaMap: mapPlane, map: musImg, trasparent: true,  side: THREE.DoubleSide,depthWrite: true }));
-  eggs[3] =com; 
-  mapItems[3] = com;
-  scene.add(com);
-  com.scale.set(20, 10,20); 
-  com.position.set(-20, 3, -60);
-  offsets[3] = {x: 5, y: 50, z: 5}; 
+//COMMUNITY
+    offsets[3] = {x: 5, y: 50, z: 5}; 
+    let pos3 = {x: -20,  y:3, z:-60 }
+    let size3 = {x: 15, y: 10}; 
+   setUpEgg(3, size3, com, pos3,musImg);
 
 //ADAPTIVE 
-    ada = new THREE.Sprite(new THREE.SpriteMaterial({ map: musImg, trasparent: true,   }));
-  eggs[4] =ada; 
-  mapItems[4] = ada;
-  scene.add(ada);
-  ada.scale.set(20, 10,20); 
-  ada.position.set(-90, 3, -80);
    offsets[4] = {x: 4, y: 50, z: 3}; 
+    let pos4 = {x: -90,  y:3, z:-80 }
+    let size4 = {x: 15, y: 10}; 
+   setUpEgg(4, size4, ada, pos4,musImg);
 
 
   //ADD TORPOEDO house CUBE
@@ -342,26 +372,49 @@ const parksImg = new THREE.TextureLoader().load("/media/map_1.png");
     offsets[7] = {x: 2, y: 40, z: 2}; 
 
 }
-
+function setUpEgg(index, size, name, pos, texture){
+  name = new THREE.Mesh(new THREE.PlaneGeometry(size.x, size.y), new THREE.MeshStandardMaterial({
+    map: texture,
+    transparent: true,
+  }));
+  name.rotation.x = -90 * (Math.PI / 180);
+  name.position.y = 3;
+  scene.add(name);
+  name.position.set(pos.x, pos.y, pos.z);
+  mapItems[index] = name;
+  eggs[index] = name; 
+}
 function makeGround(){
 //WATER + GROUND
-const map = new THREE.TextureLoader().load("/media/map@2x.png");
-const water = new THREE.TextureLoader().load("/media/water.jpg");
-  //WATER PLANE
-  var planeGeometry = new THREE.PlaneGeometry(5000, 5000, 100, 100);
-  var planeMaterial = new THREE.MeshStandardMaterial({
-    color: 0xffffbb,
-    map: water,
-    transparecy: true,
-    side: THREE.DoubleSide,
-    depthWrite: false,
-    opacity: 0.5,
-  });
-  ground = new THREE.Mesh(planeGeometry, planeMaterial);
-  ground.rotation.x = -90 * (Math.PI / 180);
-  ground.position.y = -5;
-  scene.add(ground);
 
+  const water = new THREE.TextureLoader().load("/media/water.jpg");
+    //WATER PLANE
+    var planeGeometry = new THREE.PlaneGeometry(5000, 5000, 100, 100);
+    var planeMaterial = new THREE.MeshStandardMaterial({
+      color: 0xffffbb,
+      map: water,
+      transparecy: true,
+      side: THREE.DoubleSide,
+      depthWrite: true,
+      opacity: 0.5,
+    });
+    ground = new THREE.Mesh(planeGeometry, planeMaterial);
+    ground.rotation.x = -90 * (Math.PI / 180);
+    ground.position.y = -5;
+    scene.add(ground);
+
+    ground1 = new THREE.Mesh(planeGeometry, planeMaterial);
+    ground1.rotation.x = -90 * (Math.PI / 180);
+    ground1.position.y = -20;
+    scene.add(ground1);
+
+    // ground2 = new THREE.Mesh(planeGeometry, planeMaterial);
+    // ground2.rotation.x = -90 * (Math.PI / 180);
+    // // ground2.position.x = -50;
+    // scene.add(ground2);
+// }
+
+const map = new THREE.TextureLoader().load("/media/map@2x.png");
   //MAP PLANE
   var planeGeometry = new THREE.PlaneGeometry(200, 300);
   var planeMaterial = new THREE.MeshStandardMaterial({
@@ -373,7 +426,6 @@ const water = new THREE.TextureLoader().load("/media/water.jpg");
   mapPlane = new THREE.Mesh(planeGeometry, planeMaterial);
   mapPlane.rotation.x = -90 * (Math.PI / 180);
   scene.add(mapPlane);
-
 }
 
 function onWindowResize() {
